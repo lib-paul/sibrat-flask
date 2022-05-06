f6abd1f7da3a85bad9cc3fb1f9089823cdcf0a55
Paul <libpaul@outlook.com>
2022-05-06 00:18:19 -0300

    Algunos cambios al front y a los mensajes de flask
    Se cambio el icono y el placeholder del login para hacer referencia a lo que se utiliza para el login que es el email y se cambiaron algunos formatos para expresar mejor los mensajes de flask.

--- auth/auth.py
+++ auth/auth.py
@@ -15,20 +15,39 @@ def loguearse_vista():
 def registrarse_vista():
     return render_template('register.html')
 
+@auth_bp.route('/register')
+def register():
+    
+        
 
 @auth_bp.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         # Guardo lo que vino del formulario en variables
-        username = request.form['username']
+        username = request.form['mail']
         password = request.form['password']
         logged_user = User.query.filter_by(email=username).first()
         if username == '':
             flash('Faltan el mail')
+            url_chain='auth.loguearse_vista'
         elif not logged_user:
             flash('No se encontro el usuario')
+            url_chain='auth.loguearse_vista'
         elif logged_user.password != password:
             flash('Contraseña incorrecta')
-        
-    session['username'] = logged_user.nombre_cuenta
+            url_chain='auth.loguearse_vista'
+        else:
+            session['username'] = logged_user.nombre_cuenta
+            message =  f"Bienvenido {logged_user.nombre_cuenta}"
+            flash(message)
+            url_chain='general.Index'
+        return redirect(url_for(url_chain))
+
+@auth_bp.route('/logout')
+def logout():
+    # Para elminar los datos del diccionario session
+    message = f"Hasta Luego {session['username']}"
+    session.clear()
+    # Renderiza la ruta principal
+    flash(message)
     return redirect(url_for('general.Index'))
